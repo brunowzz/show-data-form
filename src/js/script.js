150d364ac1942d06dc7d24d951ae556381721935
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-03-13 21:21:50 -0300

    feat: add logic and show data

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
 function handleForm() {
-  alert(1);
+  const form = document.querySelector(".form");
+  const result = document.querySelector(".result");
+
+  const name = form.querySelector(".name");
+  const lastName = form.querySelector(".lastName");
+  const weight = form.querySelector(".weight");
+  const height = form.querySelector(".height");
+
+  const persons = [];
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: name.value,
+      lastName: lastName.value,
+      weight: weight.value,
+      height: height.value,
+    };
+
+    persons.push({ data });
+
+    result.innerHTML += `<p>${data.name} ${data.lastName} ${data.weight} ${data.height}</p>`;
+  });
 }
 
 handleForm();
